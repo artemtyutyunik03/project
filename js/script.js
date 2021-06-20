4798e014e3bbe24151bc17f7a222030118f62762
Artem <example@gmail.com>
2021-06-20 22:48:47 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+"use strict";
+
+let answer = [];
+
+answer[0]= prompt("how old are you?","");
+answer[1]= prompt("what`s yor name?","");
+answer[2]= prompt("what`s your surname?","");
+
+// document.write(answer);
+console.log(typeof(answer));
