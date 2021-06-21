014dfa3a0e01d48ba382233c3e694945db263c2f
Artem <example@gmail.com>
2021-06-21 14:45:44 +0300

    create new attachment

--- js/script.js
+++ js/script.js
@@ -8,25 +8,26 @@ const personalMovieDB = {
   privat: false
 };
 
-// document.write(answer);
-console.log(typeof(answer));
-const a = prompt('один из последних просмотренных фильмов?', ""),
-  b = prompt('на сколько оцените его?', ""),
-  c = prompt('один из последних просмотренных фильмов?', ""),
-  d = prompt('на сколько оцените его?', "");
+for (let i = 0; i<2; i++ ){
+  const a = prompt('один из последних просмотренных фильмов?', ""),
+   b = prompt('на сколько оцените его?', "");
+  if (a !='' && b != '' && a!= null && b!= null && a.length<50 ){
+    personalMovieDB.movies[a]=b;
+  } else {
+    i--;
+  }
+}
 
-// git init --> git status --> git add -A --> git commit -a -m"" --> git push -u main 
-personalMovieDB.movies[a]=b;
-personalMovieDB.movies[c]=d;
-console.log(personalMovieDB); 
+console.log(personalMovieDB);
+
+
 const num = 50;
-
 switch(num){
   case 48: 
     console.log('false');
     break;
   case 50: 
-    console.log('true');
+    // console.log('true');
     break;
   default:
     console.log('try again');
