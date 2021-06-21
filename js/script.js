b98bee5bdfe3c69d419fd70b34acdcf59087c817
Artem <example@gmail.com>
2021-06-21 12:16:05 +0300

    create new attachment

--- js/script.js
+++ js/script.js
@@ -1,7 +1,5 @@
 "use strict";
-
-const numberOfFilms = prompt('how many films have you already seen?', "");
-
+const numberOfFilms = prompt('mcmlms');
 const personalMovieDB = {
   count: numberOfFilms,
   movies: {},
@@ -10,11 +8,27 @@ const personalMovieDB = {
   privat: false
 };
 
+// document.write(answer);
+console.log(typeof(answer));
 const a = prompt('один из последних просмотренных фильмов?', ""),
   b = prompt('на сколько оцените его?', ""),
   c = prompt('один из последних просмотренных фильмов?', ""),
   d = prompt('на сколько оцените его?', "");
 
+// git init --> git status --> git add -A --> git commit -a -m"" --> git push -u main 
 personalMovieDB.movies[a]=b;
 personalMovieDB.movies[c]=d;
-console.log(personalMovieDB);
+console.log(personalMovieDB); 
+const num = 50;
+
+switch(num){
+  case 48: 
+    console.log('false');
+    break;
+  case 50: 
+    console.log('true');
+    break;
+  default:
+    console.log('try again');
+    break;
+}
