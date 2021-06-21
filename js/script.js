4ae6b11512bf076e5f9afac1684481e757014bbe
Artem <example@gmail.com>
2021-06-21 10:47:17 +0300

    create new attachment

--- js/script.js
+++ js/script.js
@@ -1,12 +1,20 @@
 "use strict";
 
-let answer = [];
+const numberOfFilms = prompt('how many films have you already seen?', "");
 
-answer[0]= prompt("how old are you?","");
-answer[1]= prompt("what`s yor name?","");
-answer[2]= prompt("what`s your surname?","");
+const personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors:{},
+  genres:[],
+  privat: false
+};
 
-// document.write(answer);
-console.log(typeof(answer));
+const a = prompt('один из последних просмотренных фильмов?', ""),
+  b = prompt('на сколько оцените его?', ""),
+  c = prompt('один из последних просмотренных фильмов?', ""),
+  d = prompt('на сколько оцените его?', "");
 
-// git init --> git status --> git add -A --> git commit -a -m"" --> git push -u main
+personalMovieDB.movies[a]=b;
+personalMovieDB.movies[c]=d;
+console.log(personalMovieDB);
