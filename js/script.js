4a55ab26e25a7d843578cdbd6c8acc27637c27ae
Artem <example@gmail.com>
2021-06-21 08:09:20 +0300

    add comment

--- js/script.js
+++ js/script.js
@@ -8,3 +8,5 @@ answer[2]= prompt("what`s your surname?","");
 
 // document.write(answer);
 console.log(typeof(answer));
+
+// git init --> git status --> git add -A --> git commit -a -m"" --> git push -u main
